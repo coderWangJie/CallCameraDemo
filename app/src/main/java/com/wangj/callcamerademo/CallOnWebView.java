070223ee8f7ca0d35eeb0a7f5a3dcd3a3453dd6a
WangJie <jie581825@yeah.net>
2017-09-14 17:21:20 +0800

    Demo finished

--- app/src/main/java/com/wangj/callcamerademo/CallOnWebView.java
+++ app/src/main/java/com/wangj/callcamerademo/CallOnWebView.java
@@ -1,15 +1,34 @@
 package com.wangj.callcamerademo;
 
-import android.support.v7.app.AppCompatActivity;
+import android.annotation.SuppressLint;
+import android.annotation.TargetApi;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
-import android.webkit.WebResourceRequest;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.v7.app.AppCompatActivity;
+import android.text.format.DateFormat;
+import android.util.Log;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
+import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.Toast;
+
+import java.io.File;
+import java.util.Calendar;
+import java.util.Locale;
 
 public class CallOnWebView extends AppCompatActivity {
+    private int REQUEST_CODE = 1234;
     private String targetUrl = "file:///android_asset/test.html";
 
-    private WebView webView;
+    private ValueCallback<Uri[]> mUploadCallbackAboveL;
+    private ValueCallback<Uri> mUploadCallbackBelow;
+    private Uri imageUri;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,24 +36,191 @@ public class CallOnWebView extends AppCompatActivity {
         setContentView(R.layout.call_on_web_view);
         setTitle("通过WebView调用相机");
 
-        webView = (WebView) findViewById(R.id.webView);
-
         initView();
     }
 
+    @SuppressLint("SetJavaScriptEnabled")
+    @TargetApi(Build.VERSION_CODES.JELLY_BEAN)
     private void initView() {
-        webView.setWebViewClient(new WebViewClient(){
+
+        WebView webView = (WebView) findViewById(R.id.webView);
+
+        WebSettings webSetting = webView.getSettings();
+        webSetting.setJavaScriptEnabled(true);
+        webSetting.setDefaultTextEncodingName("utf-8");
+
+//        webSetting.setAllowContentAccess(true);
+//        webSetting.setAllowFileAccess(true);
+//        webSetting.setAllowFileAccessFromFileURLs(true);
+//        webSetting.setAllowUniversalAccessFromFileURLs(true);
+
+        webView.setWebViewClient(new WebViewClient() {
             @Override
-            public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-                return true;
+            public boolean shouldOverrideUrlLoading(WebView view, String url) {
+                view.loadUrl(url); // 在本WebView打开新的url请求
+                return true;  // 标记新请求已经被处理笑话
+                // 上边2行合起来，标识所有新链接都在本页面处理，不跳转别的浏览器
             }
 
             @Override
             public void onPageFinished(WebView view, String url) {
-                view.loadUrl(url);
+                super.onPageFinished(view, url);
+            }
+        });
+
+        webView.setWebChromeClient(new WebChromeClient() {
+
+            /**
+             * 8(Android 2.2) <= API <= 10(Android 2.3)回调此方法
+             */
+            public void openFileChooser(ValueCallback<Uri> uploadMsg) {
+                Log.e("WangJ", "运行方法 openFileChooser-1");
+                // (2)该方法回调时说明版本API < 21，此时将结果赋值给 mUploadCallbackBelow，使之 != null
+                mUploadCallbackBelow = uploadMsg;
+                takePhoto();
+            }
+
+            /**
+             * 11(Android 3.0) <= API <= 15(Android 4.0.3)回调此方法
+             */
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType) {
+                Log.e("WangJ", "运行方法 openFileChooser-2 (acceptType: " + acceptType + ")");
+                // 这里我们就不区分input的参数了，直接用拍照
+                openFileChooser(uploadMsg);
+            }
+
+            /**
+             * 16(Android 4.1.2) <= API <= 20(Android 4.4W.2)回调此方法
+             */
+            public void openFileChooser(ValueCallback<Uri> uploadMsg, String acceptType, String capture) {
+                Log.e("WangJ", "运行方法 openFileChooser-3 (acceptType: " + acceptType + "; capture: " + capture + ")");
+                // 这里我们就不区分input的参数了，直接用拍照
+                openFileChooser(uploadMsg);
+            }
+
+            /**
+             * API >= 21(Android 5.0.1)回调此方法
+             */
+            @Override
+            public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+                Log.e("WangJ", "运行方法 onShowFileChooser");
+                // (1)该方法回调时说明版本API >= 21，此时将结果赋值给 mUploadCallbackAboveL，使之 != null
+                mUploadCallbackAboveL = filePathCallback;
+                takePhoto();
+                return true;
             }
         });
 
         webView.loadUrl(targetUrl);
     }
+
+    /**
+     * 调用相机
+     */
+    private void takePhoto() {
+        // 指定拍照存储位置的方式调起相机
+        String filePath = Environment.getExternalStorageDirectory() + File.separator
+                + Environment.DIRECTORY_PICTURES + File.separator;
+        String fileName = "IMG_" + DateFormat.format("yyyyMMdd_hhmmss", Calendar.getInstance(Locale.CHINA)) + ".jpg";
+        imageUri = Uri.fromFile(new File(filePath + fileName));
+
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intent.putExtra(MediaStore.EXTRA_OUTPUT, imageUri);
+        startActivityForResult(intent, REQUEST_CODE);
+
+
+        // 选择图片（不包括相机拍照）,则不用成功后发刷新图库的广播
+//        Intent i = new Intent(Intent.ACTION_GET_CONTENT);
+//        i.addCategory(Intent.CATEGORY_OPENABLE);
+//        i.setType("image/*");
+//        startActivityForResult(Intent.createChooser(i, "Image Chooser"), REQUEST_CODE);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == REQUEST_CODE) {
+            // 经过上边(1)、(2)两个赋值操作，此处即可根据其值是否为空来决定采用哪种处理方法
+            if (mUploadCallbackBelow != null) {
+                chooseBelow(resultCode, data);
+            } else if (mUploadCallbackAboveL != null) {
+                chooseAbove(resultCode, data);
+            } else {
+                Toast.makeText(this, "发生错误", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    /**
+     * Android API < 21(Android 5.0)版本的回调处理
+     * @param resultCode 选取文件或拍照的返回码
+     * @param data 选取文件或拍照的返回结果
+     */
+    private void chooseBelow(int resultCode, Intent data) {
+        Log.e("WangJ", "返回调用方法--chooseBelow");
+
+        if (RESULT_OK == resultCode) {
+            updatePhotos();
+
+            if (data != null) {
+                // 这里是针对文件路径处理
+                Uri uri = data.getData();
+                if (uri != null) {
+                    Log.e("WangJ", "系统返回URI：" + uri.toString());
+                    mUploadCallbackBelow.onReceiveValue(uri);
+                } else {
+                    mUploadCallbackBelow.onReceiveValue(null);
+                }
+            } else {
+                // 以指定图像存储路径的方式调起相机，成功后返回data为空
+                Log.e("WangJ", "自定义结果：" + imageUri.toString());
+                mUploadCallbackBelow.onReceiveValue(imageUri);
+            }
+        } else {
+            mUploadCallbackBelow.onReceiveValue(null);
+        }
+        mUploadCallbackBelow = null;
+    }
+
+    /**
+     * Android API >= 21(Android 5.0) 版本的回调处理
+     * @param resultCode 选取文件或拍照的返回码
+     * @param data 选取文件或拍照的返回结果
+     */
+    private void chooseAbove(int resultCode, Intent data) {
+        Log.e("WangJ", "返回调用方法--chooseAbove");
+
+        if (RESULT_OK == resultCode) {
+            updatePhotos();
+
+            if (data != null) {
+                // 这里是针对从文件中选图片的处理
+                Uri[] results;
+                Uri uriData = data.getData();
+                if (uriData != null) {
+                    results = new Uri[]{uriData};
+                    for (Uri uri : results) {
+                        Log.e("WangJ", "系统返回URI：" + uri.toString());
+                    }
+                    mUploadCallbackAboveL.onReceiveValue(results);
+                } else {
+                    mUploadCallbackAboveL.onReceiveValue(null);
+                }
+            } else {
+                Log.e("WangJ", "自定义结果：" + imageUri.toString());
+                mUploadCallbackAboveL.onReceiveValue(new Uri[]{imageUri});
+            }
+        } else {
+            mUploadCallbackAboveL.onReceiveValue(null);
+        }
+        mUploadCallbackAboveL = null;
+    }
+
+    private void updatePhotos() {
+        // 该广播即使多发（即选取照片成功时也发送）也没有关系，只是唤醒系统刷新媒体文件
+        Intent intent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
+        intent.setData(imageUri);
+        sendBroadcast(intent);
+    }
+
 }
